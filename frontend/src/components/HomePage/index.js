391217f5c4711a8b878dd1501dce19d9ffd0af30
NawalJAhmed <11577850+NawalJAhmed@users.noreply.github.com>
2022-01-27 13:12:55 -0500

    working on splash

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -11,20 +11,6 @@ function Homepage() {
   } else {
     return (
       <>
-        <div id='hp-misc-div'>
-          <div id='quotes-container'>
-            <p id='quote1'>
-              Affordable places for you to stay at and enjoy without any compromise.
-            </p>
-            <p id='quote2'>
-            Have your own place to offer? Create a listing now!
-            </p>
-          </div>
-          <img id='home-house-img'
-            src='http://www.cla1mortgage.com/wp-content/uploads/2019/11/456-4560260_house-images-clip-art-free-clipart-house-silhouette.png'
-          ></img>
-        </div>
-
         <div id='aboutDiv'>
           <p id='about'>
             Explore all the listings available to you!
