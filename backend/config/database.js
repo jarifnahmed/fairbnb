237f915743ff07043b017358b37d191eb51a491a
“jarifnahmed” <11577850+jarifnahmed@users.noreply.github.com>
2023-01-15 11:44:39 -0500

    commented out previous development database

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,8 +6,17 @@ const password = db.password;
 const database = db.database;
 const host = db.host;
 
+
 module.exports = {
   development: {
+    // username,
+    // password,
+    // database,
+    // host,
+    // DATABASE_URL: "postgresql://jarif:jdiY4nc8P0vzoMjTwU6J8g@fairbndb-8109.7tt.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full",
+    // dialect: 'postgres',
+    // seederStorage: 'sequelize',
+
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
