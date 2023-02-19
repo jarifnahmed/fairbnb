7811b16bf5cce7c566fa3312698f720172371397
“jarifnahmed” <11577850+jarifnahmed@users.noreply.github.com>
2023-02-18 22:25:20 -0500

    new search bar

--- frontend/src/components /UserFeed/index.js
+++ frontend/src/components /UserFeed/index.js
@@ -3,13 +3,13 @@ import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { FaRegUserCircle } from 'react-icons/fa';
 import './UserFeed.css';
-// import "../SearchBad/SearchBad.css"
 
-import Search from '../Search/index';
-import SearchBad from '../SearchBad/index';
+import Searchbar from '../Searchbar/index';
 
 import FilterButton from '../FilterButton/index';
 import FilterPrice from '../FilterPrice/index';
+
+import { Center, Square, Circle } from '@chakra-ui/react'
 
 function UserFeed() {
   const { search } = window.location;
@@ -34,7 +34,6 @@ function UserFeed() {
     }
 
     return recListings.filter((listing) => {
-      const listingTitleSearch = listing.title.toLowerCase();
       const listingPriceSearch = listing.price.toString();
       const listingCitySearch = listing.city.toLowerCase();
       const listingPropertyTypeSearch = listing.propertyType.toLowerCase();
@@ -53,12 +52,11 @@ function UserFeed() {
     filterQueryPrice
   );
 
-  if (recListings.length) {
-    return (
-      <>
-        <div className='searchBarAndResults'>
-          {/* <h2 className='rec-title'>Recommended Listings</h2> */}
-          <div className='barAndFilter'>
+  // if (recListings.length) {
+  return (
+    <>
+      <div className='searchBarAndResults'>
+        {/* <div className='barAndFilter'>
             <div className='topSearchBar'>
               <Search
                 searchQuery={searchQuery}
@@ -73,123 +71,124 @@ function UserFeed() {
                 setFilterQueryPrice={setFilterQueryPrice}
               />
             </div>
-          </div>
-          <div className='searchResults'>
-            {/* <h2 className='rec-title'>
-              {' '}
-              There {document.querySelectorAll(".feed-list").length > 1 ? 'are' : 'is'} {document.querySelectorAll(".feed-list").length}{' '}
-          {document.querySelectorAll(".feed-list").length > 1 ? 'listings' : 'listing'}.
-            </h2> */}
-            <ul className='allCards'>
-              {recListings.map((listing) => {
-                let d = new Date(listing.createdAt);
-                let dateWritten = d.toString().slice(4, 10);
-                if (
-                  listing.propertyType === filterQuery &&
-                  listing.price <= filterQueryPrice
-                ) {
-                  return (
-                    <li key={listing.id} className='feed-list'>
-                      <NavLink
-                        className='listing-link'
-                        to={`/listings/${listing.id}`}
-                      >
-                        <div className='neumorphic-card mx-auto'>
-                          <div className='neumorphic-card__outer'>
-                            <img
-                              className='neumorphic-image'
-                              src={listing.imageUrl[0]}
-                              alt='listing'
-                            />
-                            <p className='neumorphic-card__title'>
-                              {listing.city.slice(0, -5)}
-                            </p>
-                            <div className='propertyTypeAndPriceLine'>
-                              <p className='neumorphic-card__text'>
-                                {listing.propertyType}
-                              </p>
-                              <p className='neumorphic-card__text'>
-                                $
-                                {listing.price === 0
-                                  ? listing.price + 1
-                                  : listing.price}{' '}
-                                / night
-                              </p>
-                            </div>
-                            {/* <p className='neumorphic-card__text'>{dateWritten}</p> */}
-                          </div>
-                        </div>
-                      </NavLink>
-                    </li>
-                  );
-                } else if (
-                  filterQuery === '' &&
-                  listing.price <= filterQueryPrice
-                ) {
-                  return (
-                    <li key={listing.id} className='feed-list'>
-                      <NavLink
-                        className='listing-link'
-                        to={`/listings/${listing.id}`}
-                      >
-                        <div className='neumorphic-card mx-auto'>
-                          <div className='neumorphic-card__outer'>
-                            <img
-                              className='neumorphic-image'
-                              src={listing.imageUrl[0]}
-                              alt='listing'
-                            />
-                            <p className='neumorphic-card__title'>
-                              {listing.city.slice(0, -5)}
-                            </p>
-                            <div className='propertyTypeAndPriceLine'>
-                              <p className='neumorphic-card__text'>
-                                {listing.propertyType}
-                              </p>
-                              <p className='neumorphic-card__text'>
-                                $
-                                {listing.price === 0
-                                  ? listing.price + 1
-                                  : listing.price}{' '}
-                                / night
-                              </p>
-                            </div>
-                            {/* <p className='neumorphic-card__text'>{dateWritten}</p> */}
-                          </div>
-                        </div>
-                      </NavLink>
-                    </li>
-                  );
-                }
-              })}
-            </ul>
-          </div>
-        </div>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <div className='barAndFilter'>
-          <div className='topSearchBar'>
-            <SearchBad
+          </div> */}
+        <div>
+          <Center>
+            <Searchbar
               searchQuery={searchQuery}
               setSearchQuery={setSearchQuery}
             />
-            <FilterButton
-              filterQuery={filterQuery}
-              setFilterQuery={setFilterQuery}
-            />
-            <FilterPrice
-              filterQueryPrice={filterQueryPrice}
-              setFilterQueryPrice={setFilterQueryPrice}
-            />
-          </div>
+          </Center>
+          <Center>{recListings.length} Results</Center>
+
+          <ul className='allCards'>
+            {recListings.map((listing) => {
+              let d = new Date(listing.createdAt);
+              let dateWritten = d.toString().slice(4, 10);
+              if (
+                listing.propertyType === filterQuery &&
+                listing.price <= filterQueryPrice
+              ) {
+                return (
+                  <li key={listing.id} className='feed-list'>
+                    <NavLink
+                      className='listing-link'
+                      to={`/listings/${listing.id}`}
+                    >
+                      <div className='neumorphic-card mx-auto'>
+                        <div className='neumorphic-card__outer'>
+                          <img
+                            className='neumorphic-image'
+                            src={listing.imageUrl[0]}
+                            alt='listing'
+                          />
+                          <p className='neumorphic-card__title'>
+                            {listing.city.slice(0, -5)}
+                          </p>
+                          <div className='propertyTypeAndPriceLine'>
+                            <p className='neumorphic-card__text'>
+                              {listing.propertyType}
+                            </p>
+                            <p className='neumorphic-card__text'>
+                              $
+                              {listing.price === 0
+                                ? listing.price + 1
+                                : listing.price}{' '}
+                              / night
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </NavLink>
+                  </li>
+                );
+              } else if (
+                filterQuery === '' &&
+                listing.price <= filterQueryPrice
+              ) {
+                return (
+                  <li key={listing.id} className='feed-list'>
+                    <NavLink
+                      className='listing-link'
+                      to={`/listings/${listing.id}`}
+                    >
+                      <div className='neumorphic-card mx-auto'>
+                        <div className='neumorphic-card__outer'>
+                          <img
+                            className='neumorphic-image'
+                            src={listing.imageUrl[0]}
+                            alt='listing'
+                          />
+                          <p className='neumorphic-card__title'>
+                            {listing.city.slice(0, -5)}
+                          </p>
+                          <div className='propertyTypeAndPriceLine'>
+                            <p className='neumorphic-card__text'>
+                              {listing.propertyType}
+                            </p>
+                            <p className='neumorphic-card__text'>
+                              $
+                              {listing.price === 0
+                                ? listing.price + 1
+                                : listing.price}{' '}
+                              / night
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </NavLink>
+                  </li>
+                );
+              }
+            })}
+          </ul>
         </div>
-        <h2 className='rec-title'>No Listings Match Current Search</h2>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
+  // } else {
+  // return (
+  //   <>
+  //     <div className='barAndFilter'>
+  //       <div className='topSearchBar'>
+  //         <SearchBad
+  //           searchQuery={searchQuery}
+  //           setSearchQuery={setSearchQuery}
+  //         />
+  //         <FilterButton
+  //           filterQuery={filterQuery}
+  //           setFilterQuery={setFilterQuery}
+  //         />
+  //         <FilterPrice
+  //           filterQueryPrice={filterQueryPrice}
+  //           setFilterQueryPrice={setFilterQueryPrice}
+  //         />
+  //       </div>
+  //     </div>
+  //     <h2 className='rec-title'>No Listings Match Current Search</h2>
+  //   </>
+  // );
 }
+// }
 
 export default UserFeed;
